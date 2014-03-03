0b78c0284c3948ef78e1765d948b19be234c76aa
Asia wang <ouyazhou@wangyazhoudemacbookAir.local>
2014-03-03 22:20:26 +0800

    Initial Commit

---
+++ lianxi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lianxi
+//
+//  Created by Asia wang on 14-3-3.
+//  Copyright (c) 2014年 wangyazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ lianxiTests/lianxiTests.m
@@ -0,0 +1,34 @@
+//
+//  lianxiTests.m
+//  lianxiTests
+//
+//  Created by Asia wang on 14-3-3.
+//  Copyright (c) 2014年 wangyazhou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lianxiTests : XCTestCase
+
+@end
+
+@implementation lianxiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
